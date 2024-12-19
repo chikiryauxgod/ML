e114d501d2c5e23e7d461f149e3ee4fcbef1a90d
Семенов Лев <lewace37@yandex.ru>
2024-12-19 19:47:48 +0500

    add virtual environment and absolute paths

--- pets/model.py
+++ pets/model.py
@@ -1,5 +1,7 @@
 if __name__ == "__main__":
     from ultralytics import YOLO
+    BASE_DIR = Path(__file__).resolve().parent
 
+    yaml_path = BASE_DIR / "pets" / "petsconfig.yaml"
     model = YOLO("yolov8n.pt")
-    results = model.train(data="D:\\GitHub\\ML\\pets\\petsconfig.yaml", epochs=100, imgsz=384)
+    results = model.train(data= yaml_path, epochs=100, imgsz=384)
--- validationtest/validation.py
+++ validationtest/validation.py
@@ -1,42 +1,43 @@
+from pathlib import Path
 from ultralytics import YOLO
 import cv2
 
-model = YOLO("D:\\GitHub\\ML\\validationtest\\best.pt")  
-video_path = "D:\\GitHub\\ML\\validationtest\\dog.mp4"
-cap = cv2.VideoCapture(video_path)
+BASE_DIR = Path(__file__).resolve().parent
 
+model_path = BASE_DIR / "validationtest" / "best.pt"
+video_path = BASE_DIR / "validationtest" / "dog.mp4"
+output_path = BASE_DIR / "validationtest" / "dog_annotated.mp4"
+
+model = YOLO(str(model_path))
+
+cap = cv2.VideoCapture(str(video_path))
 if not cap.isOpened():
-    print("Error.")
+    print("Error: Unable to open video.")
     exit()
-
 
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 process_fps = 10  
-frame_interval = int(fps / process_fps) 
+frame_interval = int(fps / process_fps)
 
-
-output_path = "D:\\GitHub\\ML\\validationtest\\dog_annotated.mp4"
 out = cv2.VideoWriter(
-    output_path, 
+    str(output_path), 
     cv2.VideoWriter_fourcc(*'mp4v'), 
     fps, 
     (frame_width, frame_height)
 )
 
-
 frame_count = 0
 while True:
     ret, frame = cap.read()
-    if not ret: 
+    if not ret:
         break
 
     if frame_count % frame_interval == 0:
         results = model(frame)
         annotated_frame = results[0].plot()
     else:
-        
         annotated_frame = frame
 
     cv2.imshow("YOLOv8 Detection", annotated_frame)
