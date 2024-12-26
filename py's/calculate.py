9f821e01f7e1de461f3f18ecafd4c95621740a38
Lev <130220311+chikiryauxgod@users.noreply.github.com>
2024-12-26 12:39:10 +0500

    remake

--- py's/calculate.py
+++ py's/calculate.py
@@ -2,11 +2,11 @@
 # Поступают данные с гирокомпаса (1 координата, скорость вращения Z)
 # Данные имеют:
 # а) Неизвестный линейный дрейф - при неподвижному состоянии думает, что двигается
-# б) гауссовский шум (параметры на выбор)
+# б) Гауссовский шум (параметры на выбор)
 
 # Написать фильтр, который:
-# а) уберет дрейф, определив его скорость по "неподвижному основанию" 
-# б) Уберет шум, основываясь на знании параметров шума
+# а) Уберет дрейф, определив его скорость по "неподвижному основанию" 
+# б) Уберет шум, основываясь на знани и параметров шума
 # 
 
 import numpy as np
@@ -17,7 +17,7 @@ def generate_data(mean, dev, size):
 
 def generate_speed(accelerations, dt):
     speeds = np.zeros_like(accelerations)
-    for i in range(1, len(accelerations)):
+    for i in range(len(accelerations)):
         speeds[i] = speeds[i - 1] + accelerations[i] * dt
     return speeds
 
@@ -27,10 +27,8 @@ def remove_drift(speeds, threshold):
     corrected_speeds = speeds - drift
     return corrected_speeds, drift 
 
-def smooth_data(data, window_size):
-    return np.convolve(data, np.ones(window_size) / window_size, mode='same')
 
-def print_data(acc, speeds, corr, smooth):
+def print_data(acc, speeds, corr): 
     plt.subplot(4, 1, 1)
     plt.plot(acc, label="Accelerations", color="blue")
     plt.title("Accelerations")
@@ -49,12 +47,6 @@ def print_data(acc, speeds, corr, smooth):
     plt.legend()
     plt.grid()
 
-    plt.subplot(4, 1, 4)
-    plt.plot(smooth, label="Smoothed Speeds", color="red")
-    plt.title("Smoothed Speeds")
-    plt.legend()
-    plt.grid()
-
     plt.tight_layout()
     plt.show()
 
@@ -65,15 +57,10 @@ def main():
     accelerations = generate_data(mean, dev, size)
     dt = 0.1
     speeds = generate_speed(accelerations, dt)
-    # print(f"First ten accelerations: {accelerations[:10]}")
-    # print(f"First ten speeds: {speeds[:10]}")
 
     threshold = 0.05
     corrected_speeds, drift = remove_drift(speeds, threshold)
-    
-    window_size = 50
-    smoothed_speeds = smooth_data(corrected_speeds, window_size)
-    print_data(accelerations, speeds, corrected_speeds, smoothed_speeds)
+    print_data(accelerations, speeds, corrected_speeds)
 
 if __name__ == "__main__":
    main()
