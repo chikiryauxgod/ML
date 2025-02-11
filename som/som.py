70066ce4c60d6c8651098447252fd02dd1046927
Семенов Лев <lewace37@yandex.ru>
2025-02-11 23:31:50 +0500

    upd

--- som/som.py
+++ som/som.py
@@ -2,31 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from minisom import MiniSom
 from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
 
-def get_data(): 
-    return np.random.rand(500, 3)  
+def get_weather_data():
+    file_path = 'som/weather_data.csv'
+    weather_data = pd.read_csv(file_path)
+    return weather_data
 
 def normalize(data):
     scaler = MinMaxScaler()
     return scaler.fit_transform(data)
 
 def init_som(data):
-    som = MiniSom(20, 20, data.shape[1], sigma=1.0, learning_rate=0.5)
+    som = MiniSom(100, 100, data.shape[1], sigma=1.0, learning_rate=0.5)
     som.random_weights_init(data)
     som.train_random(data, 1000)
     return som
 
-def visualize():
-    plt.figure(figsize=(8, 8))
-    u_matrix = som.distance_map().T  # transponised
+def visualize(som, data):
+    plt.figure(figsize=(12, 12))
+    u_matrix = som.distance_map().T 
     plt.imshow(u_matrix, cmap='bone_r')
-    plt.colorbar(label='Среднее расстояние')
-    plt.title('U-Matrix для SOM')
+    plt.colorbar(label='AVG distance:')
+    plt.title('SOM') 
     plt.show()
 
-
 if __name__ == '__main__':
-    data = get_data()
-    data = normalize(data)
-    som = init_som(data)
-    visualize()
+    weather_data = get_weather_data()
+    selected_features = weather_data[['Outdoor Drybulb Temperature [C]', 'Outdoor Relative Humidity [%]', 
+                                      'Diffuse Solar Radiation [W/m2]', 'Direct Solar Radiation [W/m2]']]
+                                    # Рассеянная и прямая солнечная радиациЯ
+    normalized_data = normalize(selected_features)
+    som = init_som(normalized_data)
+    visualize(som, normalized_data)
